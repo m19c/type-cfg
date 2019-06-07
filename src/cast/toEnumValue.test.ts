6733dd3ed9b8fdac6f4e342a60afe19e13534f9a
m19c <marcandrebinder@gmail.com>
2019-06-07 11:39:10 +0200

    bump version to 1.0.0

--- src/cast/toEnumValue.test.ts
+++ src/cast/toEnumValue.test.ts
@@ -8,4 +8,13 @@ describe('cast/toEnumValue', () => {
   test('throws an error if the value is not present', () => {
     expect(() => toEnumValue({ A: 1 }, 2)).toThrow();
   });
+
+  test('works with a real world example', () => {
+    enum Environment {
+      Development = 'development',
+      Production = 'production',
+    }
+    expect(toEnumValue(Environment, 'development')).toBe(Environment.Development);
+    expect(toEnumValue(Environment, 'production')).toBe(Environment.Production);
+  });
 });
